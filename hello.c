e06265b87a594c54f121293b0414c2be9c60e669
sstott711 <46725337+sstott711@users.noreply.github.com>
2019-01-17 12:58:50 -0600

    Initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+* Author: Sam Stott
+* Date: 2019/1/17
+*
+* A simple hello world program in C
+*
+*/
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv)
+{
+  printf ("Sam Stott\n");
+  printf ("Marketing\n");
+  
+  return 0
+}
